4d02407fb89b8996de83ce2590e90d2091f3cf71
DESKTOP-ID0V821\leduytuanvu <leduytuanvu.work@gmail.com>
2025-01-18 14:42:54 +0700

    [leduytuanvu] update src code

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000,
         });
         console.log('Database connected successfully');
     } catch (error) {
