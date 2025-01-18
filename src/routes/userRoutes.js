6c33bc6e0cb8d856fc7535777991153746bd33ed
DESKTOP-ID0V821\leduytuanvu <leduytuanvu.work@gmail.com>
2025-01-18 15:44:24 +0700

    [leduytuanvu] update src code

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,6 +5,33 @@ const router = express.Router();
 
 /**
  * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Retrieve a list of all users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Internal server error
+ */
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+/**
+ * @swagger
  * /api/users/login:
  *   post:
  *     summary: Login a user
@@ -51,7 +78,7 @@ router.post('/login', async (req, res) => {
             return res.status(401).json({ message: 'Invalid email or password' });
         }
 
-        const isMatch = await user.matchPassword(password);
+        const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(401).json({ message: 'Invalid email or password' });
         }
